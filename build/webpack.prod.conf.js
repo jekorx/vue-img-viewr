88d5a0749cda62e4bdc15aa992612d92ea28c962
‘jekorx’ <wdg404@163.com>
2020-11-20 09:35:23 +0800

    externals vue

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -21,6 +21,9 @@ module.exports = merge(baseWebpackConfig, {
     library: 'VueImgViewr',
     libraryTarget: 'commonjs2'
   },
+  externals: {
+    vue: 'vue'
+  },
   performance: {
     hints: false
   },
