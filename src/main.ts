5e6f31400da453cc151e6f0b9f062b43515aaea2
¡®jekorx¡¯ <wdg404@163.com>
2021-06-16 15:13:57 +0800

    new version v2.0.0 with vue@3.x

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './app.vue'
-import { showImages } from './img-viewr'
-
-const app = createApp(App)
-
-app.config.globalProperties.$showImages = showImages
-
-app.mount('#app')
